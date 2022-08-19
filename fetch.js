3632d72554f15adc2d98e1afd22c54db4d1b7512
speria846 <niwenyesigasperia@gmail.com>
2022-08-19 17:35:01 +0300

    first commit

--- fetch.js
+++ fetch.js
@@ -1,6 +1,24 @@
-function getproducts(){
-    fetch('localhost:5000/products')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
+const products = document.getElementById("products");
+const fruList = document.getElementById("fruList");
+const vegList = document.getElementById("vegList");
+
+async function getobjects(item,element){
+  const response = await fetch("http://localhost:5000/"+item).then((response)=>{
+   return response.json();
+   }).then((data)=>{ data.forEach((product) => {
+     const li = document.createElement("li");
+     li.innerHTML = product["name"].concat("<br/>").concat(product["category"]);
+     element.appendChild(li);
+   });
+ })
 }
-getproducts()
+
+//Products
+getobjects("products",products);
+
+//fruits
+getobjects("products/fruits",fruList);
+
+//vegetables
+getobjects("products/vegetables",vegList);
